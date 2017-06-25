5c054537578c68d1e484e7ae95f2648c8677b9d2
poiuytrewq0987654321 <bundushathurh@gmail.com>
2017-06-25 10:53:56 +0200

    jqury
    slajdy przed wyodrebnieniem funkcji

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,56 @@
 
 //console.log($('p').text());
 
-$(function(){
+$(function () {
     /* zmienne */
     var slideCount = $('.single-slide').length;
     var slideShow = $('.slide-show');
-    var slideWidth = 100/slideCount;
-    
+    var slideWidth = 100 / slideCount;
+    var slideIndex = 0;
+
     /* automatyczna szerokosc elementu w zaleznosci od ilosci elementow w srodku */
-    slideShow.css('width',slideCount * 100 +'%');
+    slideShow.css('width', slideCount * 100 + '%');
     console.log(slideCount);
-    
+
     /* szerokosc i położenie pojedynczego slajdu */
-    
-    $('.single-slide').each(function(index){
-        $(this).css({'width': slideWidth + '%','margin-left':index * slideWidth + '%'});
+
+    $('.single-slide').each(function (index) {
+        $(this).css({
+            'width': slideWidth + '%',
+            'margin-left': index * slideWidth + '%'
+        });
     })
-    
+
+    /* klikniecie w strzalki */
+    $('.prev-slide').click(function () {
+        var newSlideIndex = slideIndex - 1;
+
+        if (newSlideIndex < 0) {
+            return;
+        }
+
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        slideShow.animate({
+            'margin-left': marginLeft
+        }, 500, function () {
+            slideIndex = newSlideIndex;
+        });
+    });
+
+    $('.next-slide').click(function () {
+        var newSlideIndex = slideIndex + 1;
+
+        if (newSlideIndex >= slideCount) {
+            return;
+        }
+
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        slideShow.animate({
+            'margin-left': marginLeft
+        }, 500, function () {
+            slideIndex = newSlideIndex;
+        });
+    });
+
+ 
 });
